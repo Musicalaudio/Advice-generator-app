c19ca751ed7221bfe4f08a75f24ebb14f9559448
Jordan D'Souza <jordandsouza352@gmail.com>
2023-01-17 22:10:07 +1300

    added no-cache to fetch request

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-document.querySelector(".btn").addEventListener("click", ()=>{
+document.querySelector(".btn").addEventListener("click", getAdvice)
+
+
+function getAdvice () {
   console.log('hi')
-  fetch("https://api.adviceslip.com/advice")
+  fetch(`https://api.adviceslip.com/advice`, {cache: "no-cache"})
   .then(res => res.json())
   .then(data => {
     console.log(data.slip["advice"])
-    document.querySelector(".card__advice").textContent = data.slip["advice"]
-    document.querySelector(".card__advice-number").textContent = data.slip["id"]
+    document.querySelector(".card__advice").textContent = data.slip.advice
+    document.querySelector(".card__advice-number").textContent = data.slip.id
   }).catch(err =>{
     console.log(`Error: ${err}`)
   })
-})
+}
+
+
+window.onload = () =>{
+  getAdvice();
+}
